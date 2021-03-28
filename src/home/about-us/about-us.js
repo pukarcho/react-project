2114b5503e030209633d8e6694afdcda97233761
pukarcho <momchil.georgiev@reconart.com>
2021-03-28 18:31:15 +0300

    AboutUs page title fix

--- src/home/about-us/about-us.js
+++ src/home/about-us/about-us.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Row, Col, Tabs } from 'antd';
 
 import './about-us.css'
@@ -5,13 +7,28 @@ import './about-us.css'
 const { TabPane } = Tabs;
 
 function AboutUs() {
+    const [pageTitle, setPageTitle] = useState('About Us');
+
+    const onTabChange = (event) => {
+        switch(event){
+            case 'aboutus': 
+                setPageTitle('About Us');
+                break
+            case 'contacts': 
+                setPageTitle('Contacts');
+                break
+            default:
+                setPageTitle('About Us');
+        }
+    };
 
     return (
         <Row>
             <Col span={12} offset={6}>
+                <h3 style={{textAlign: "center"}}>{pageTitle}</h3>
                 <div className="about-us-wrapper">
-                    <Tabs tabPosition="left" style={{minHeight: "200px"}}>
-                        <TabPane tab="About us" key="1">
+                    <Tabs tabPosition="left" onChange={onTabChange} style={{minHeight: "200px"}}>
+                        <TabPane tab="About us" key="aboutus">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -30,7 +47,7 @@ function AboutUs() {
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </TabPane>
-                        <TabPane tab="Contacts" key="2">
+                        <TabPane tab="Contacts" key="contacts">
                             Veliko Tarnovo
                         </TabPane>
                     </Tabs>
